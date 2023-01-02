b1c4bc66f38d2115768b0969ff98ac64fe0fa82c
vivekkhudaniya <vivekkhudaniya@gmail.com>
2023-01-02 23:48:19 +0530

    ethdaddy initial commit

---
+++ contracts/ethdaddy/Ethdaddy.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract ETHDaddy is ERC721 {
+    uint256 public maxSupply;
+    uint256 public totalSupply;
+    address public owner;
+
+    struct Domain {
+        string name;
+        uint256 cost;
+        bool isOwned;
+    }
+
+    mapping(uint256 => Domain) domains;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    constructor(string memory _name, string memory _symbol)
+        ERC721(_name, _symbol)
+    {
+        owner = msg.sender;
+    }
+
+    function list(string memory _name, uint256 _cost) public onlyOwner {
+        maxSupply++;
+        domains[maxSupply] = Domain(_name, _cost, false);
+    }
+
+}
