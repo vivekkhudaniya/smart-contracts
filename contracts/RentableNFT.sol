c2c0a847bff28018b076dd34db69745895176f83
vivekkhudaniya <vivekkhudaniya@gmail.com>
2023-02-21 23:56:00 +0530

    RentableContract #2

--- contracts/RentableNFT.sol
+++ contracts/RentableNFT.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
 import "./ERC4907";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
@@ -28,6 +30,29 @@ contract RentableNFT is ERC4907,Ownable{
         rentables[tokenId] = RentableItems(false,baseAmount);
     }
 
-    
+    function setRentFee(uint _tokenId, bool _amountPerMinute) public {
+        require(_isApprovedOrOwner(msg.sender,_tokenId),"caller is not token owner or approved");
+        rentables[_tokenId].amountPerMinute = _amountPerMinute;
+    }
 
+    function setRentFee(uint _tokenId, bool _isRentable) public {
+        require(_isApprovedOrOwner(msg.sender,_tokenId),"caller is not token owner or approved");
+        rentables[_tokenId].isRentable = _isRentable;
+    }
+
+    function rent(uint _tokenId, uint64 _expires) public payable{
+        uint amount = rentables[_tokenId].amountPerMinute * _expires;
+        require(msg.value == amount,"Insufficient funds");
+        require(rentables[_tokenId].isRentable,"not disabled by the owner");
+        require(userOf[_tokenId] == address(0),"already minted");
+
+        payable(OwnerOf(_tokenId)).transfer(amount);
+
+        UserInfo storage info = _users[_tokenId];
+
+        info.user = msg.sender;
+        info._expires = block.timestamp + expires * 60;
+
+        
+    }
 }
