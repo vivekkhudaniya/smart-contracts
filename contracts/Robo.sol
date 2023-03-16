168685184236aae7e4bdea6904cb9a252a1987e0
vivekkhudaniya <vivekkhudaniya@gmail.com>
2023-03-17 00:21:04 +0530

    RoboPunksNFT

--- contracts/Robo.sol
+++ contracts/Robo.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -12,4 +12,22 @@ contract RoboPunksNFT is ERC721,Ownable{
     uint256 public maxPerWallet;
     string internal baseTokenURI;
     address payable public withdrawWallet;
+    mapping(address => uint256) public walletMint;
+
+    constructor() payable ERC721 ("RoboPunksNFT","RB"){
+        minPrice = 0.2 ether;
+        totalSupply = 0;
+        maxSupply = 1000;
+        maxPerWallet = 3;
+        //set withdraw wallet address
+    }
+
+    function setBaseTokenURI(string calldata _baseTokenURI) external onlyOwner{
+        baseTokenURI = _baseTokenURI;
+    }
+
+    function tokenURI(uint256 _tokenURI) public view override returns(string memory){
+        require(_exists(_tokenURI),'Token does not exits');
+        return string(abi.encodePacked(baseTokenURI, Strings.tostring(_tokenURI), ".json"));
+    }
 }
