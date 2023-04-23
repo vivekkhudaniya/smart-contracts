781e4895abae8685b87cd453b925f50968920efb
vivekkhudaniya <vivekkhudaniya@gmail.com>
2023-04-24 01:22:35 +0530

    Call data

--- contracts/StorageMemory.sol
+++ contracts/StorageMemory.sol
@@ -10,19 +10,23 @@ contract TestStorage{
 
     mapping(address => MyStruct) public myStructs;
 
-    function setVariable() public {
+    function setVariable(uint[] calldata y) public {
         myStructs[msg.sender] = MyStruct({num : 123,foo :"Vivek"});
 
         MyStruct storage myStruct = myStructs[msg.sender];
 
         myStruct.num = 456;
-
+        _internal(y);
         MyStruct memory myStruc= myStructs[msg.sender];
         
         myStruc.num = 123;
 
 
 
+    }
+
+    function _internal(uint[] calldata y) private{
+        uint x = y[0];
     }
 
 }
